6e648b4baa372728edfb7fea2233aa9a434689d7
Peter Francis <francisp336@gmail.com>
2020-03-21 00:17:28 -0400

    change score to 1 and 2

--- js/score.js
+++ js/score.js
@@ -1,20 +1,20 @@
-function up_white() {
-  var curr = Number(document.getElementById("white-score").innerHTML);
-  document.getElementById("white-score").innerHTML = curr + 1;
+function up_1() {
+  var curr = Number(document.getElementById("1-score").innerHTML);
+  document.getElementById("1-score").innerHTML = curr + 1;
 }
-function up_black() {
-  var curr = Number(document.getElementById("black-score").innerHTML);
-  document.getElementById("black-score").innerHTML = curr + 1;
+function up_2() {
+  var curr = Number(document.getElementById("2-score").innerHTML);
+  document.getElementById("2-score").innerHTML = curr + 1;
 }
-function down_white() {
-  var curr = Number(document.getElementById("white-score").innerHTML);
-  document.getElementById("white-score").innerHTML =  curr > 0 ? curr - 1 : curr;
+function down_1() {
+  var curr = Number(document.getElementById("1-score").innerHTML);
+  document.getElementById("1-score").innerHTML =  curr > 0 ? curr - 1 : curr;
 }
-function down_black() {
-  var curr = Number(document.getElementById("black-score").innerHTML);
-  document.getElementById("black-score").innerHTML =  curr > 0 ? curr - 1 : curr;
+function down_2() {
+  var curr = Number(document.getElementById("2-score").innerHTML);
+  document.getElementById("2-score").innerHTML =  curr > 0 ? curr - 1 : curr;
 }
 function reset_scores() {
-  document.getElementById("white-score").innerHTML =  0;
-  document.getElementById("black-score").innerHTML =  0;
+  document.getElementById("1-score").innerHTML =  0;
+  document.getElementById("2-score").innerHTML =  0;
 }
