389e3498901a1e77f2b68e44d46973ddd1e8d1e3
Peter E. Francis <franpe02@gettysburg.edu>
2020-06-23 15:55:07 -0400

    fix play/pause

--- js/music.js
+++ js/music.js
@@ -1,29 +1,33 @@
 
 
 var audio = new Audio('Sedario.mp3');
-var playing = false;
 audio.volume = 0.1;
 audio.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
 
-function toggle_music() {
-  if (playing) {
-    audio.pause();
-    document.getElementById("music-button").innerHTML = 'PLAY';
-    playing = false;
+audio.addEventListener('pause', function() {
+	document.getElementById("music-button").innerHTML = 'PLAY';
+});
 
+audio.addEventListener('play', function() {
+	document.getElementById("music-button").innerHTML = 'PAUSE';
+});
+
+function toggle_music() {
+  if (audio.paused) {
+		audio.play();
+		document.getElementById("music-button").innerHTML = 'PAUSE';
   }
   else {
-    audio.play();
-    document.getElementById("music-button").innerHTML = 'PAUSE';
-    playing = true;
+		audio.pause();
+		document.getElementById("music-button").innerHTML = 'PLAY';
   }
 }
 
 function start_music() {
-  if (!playing && audio.currentTime == 0) {
+  if (audio.paused && audio.currentTime == 0) {
     toggle_music();
   }
 }
